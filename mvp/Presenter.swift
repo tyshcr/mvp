bcc742976f8f407c9255bbae9a2c6c824e1bf441
TYSH, CHRISTOPHER <ctysh@monsanto.com>
2017-02-15 16:36:47 -0600

    Moved data manipulation out of View Controller and into Presenter; Added spinner;

--- mvp/Presenter.swift
+++ mvp/Presenter.swift
@@ -8,29 +8,48 @@
 
 import Foundation
 
+struct TeamViewData {
+    let fullName: String
+    let wins: Int
+    let losses: Int
+}
+
 protocol PresenterDelegate {
-    func setupData(incomingData: [Team])
+    func startLoading()
+    func finishLoading()
+    func setTeams(incomingData: [TeamViewData])
 }
 
 class Presenter {
     var delegate: PresenterDelegate?
-    var data = [Team]()
+    var data = [TeamViewData]()
     
     init() { }
     
     func getData() {
+        delegate?.startLoading()
+        
         data.removeAll()
         
-        // TODO: Fetch JSON from file 
+        // TODO: Fetch JSON from file / service
         let team1 = Team(city: "Cleveland", nickname: "Cavaliers", wins: 29, losses: 11)
         let team2 = Team(city: "Toronto", nickname: "Raptors", wins: 27, losses: 13)
         let team3 = Team(city: "Boston", nickname: "Celtics", wins: 26, losses: 15)
         
-        data.append(team1)
-        data.append(team2)
-        data.append(team3)
+        // TODO: Loop through the data and format it for the view
+        let team1data = TeamViewData(fullName: teamName(city: team1.city, nickname: team1.nickname), wins: team1.wins, losses: team1.losses)
+        let team2data = TeamViewData(fullName: teamName(city: team2.city, nickname: team2.nickname), wins: team2.wins, losses: team2.losses)
+        let team3data = TeamViewData(fullName: teamName(city: team3.city, nickname: team3.nickname), wins: team3.wins, losses: team3.losses)
         
-        delegate?.setupData(incomingData: data)
+        data.append(team1data)
+        data.append(team2data)
+        data.append(team3data)
+        
+        delegate?.finishLoading()
+        delegate?.setTeams(incomingData: data)
     }
     
+    func teamName(city: String, nickname: String) -> String {
+        return "\(city) \(nickname)"
+    }
 }
--- mvp/ViewController.swift
+++ mvp/ViewController.swift
@@ -2,8 +2,8 @@
 //  ViewController.swift
 //  mvp
 //
-//  Created by TYSH, CHRISTOPHER R [AG-Contractor/1000] on 1/11/17.
-//  Copyright © 2017 TYSH, CHRISTOPHER R [AG-Contractor/1000]. All rights reserved.
+//  Created by TYSH, CHRISTOPHER R on 1/11/17.
+//  Copyright © 2017 TYSH, CHRISTOPHER R 
 //
 
 import UIKit
@@ -12,8 +12,9 @@ class ViewController: UIViewController {
 
     private var presenter: Presenter!
     @IBOutlet weak var tableView: UITableView?
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
     
-    var processedData = [Team]()
+    var processedData = [TeamViewData]()
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
@@ -33,6 +34,7 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
     }
     
 
@@ -47,7 +49,7 @@ extension ViewController: UITableViewDataSource {
         let cell = UITableViewCell(style: UITableViewCellStyle.subtitle, reuseIdentifier: "TeamCell")
         let currentCellData = processedData[indexPath.row]
         
-        cell.textLabel?.text = currentCellData.city
+        cell.textLabel?.text = currentCellData.fullName
         cell.detailTextLabel?.text = String(currentCellData.wins) + " - " + String(currentCellData.losses)
         
         return cell
@@ -55,11 +57,17 @@ extension ViewController: UITableViewDataSource {
 }
 
 extension ViewController: PresenterDelegate {
-    func setupData(incomingData: [Team]) {
-
-        for team in incomingData {
-           processedData.append(team)
-        }
+    func startLoading() {
+        spinner.startAnimating()
+    }
+    
+    func finishLoading() {
+        spinner.stopAnimating()
+    }
+    
+    func setTeams(incomingData: [TeamViewData]) {
+        processedData = incomingData
+        self.tableView?.reloadData()
     }
 
 }
