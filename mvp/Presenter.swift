e406c067dd3da875e51b3681d2769a695227a069
tyshcr <chris@feltpad.net>
2017-02-17 10:28:50 -0600

    Add method to calculate win percentage

--- mvp/Presenter.swift
+++ mvp/Presenter.swift
@@ -12,6 +12,7 @@ struct TeamViewData {
     let fullName: String
     let wins: Int
     let losses: Int
+    let percentage: String
 }
 
 protocol PresenterDelegate {
@@ -37,9 +38,9 @@ class Presenter {
         let team3 = Team(city: "Boston", nickname: "Celtics", wins: 26, losses: 15)
         
         // TODO: Loop through the data and format it for the view
-        let team1data = TeamViewData(fullName: teamName(city: team1.city, nickname: team1.nickname), wins: team1.wins, losses: team1.losses)
-        let team2data = TeamViewData(fullName: teamName(city: team2.city, nickname: team2.nickname), wins: team2.wins, losses: team2.losses)
-        let team3data = TeamViewData(fullName: teamName(city: team3.city, nickname: team3.nickname), wins: team3.wins, losses: team3.losses)
+        let team1data = TeamViewData(fullName: teamName(city: team1.city, nickname: team1.nickname), wins: team1.wins, losses: team1.losses, percentage: winPercentage(wins: team1.wins, losses: team1.losses))
+        let team2data = TeamViewData(fullName: teamName(city: team2.city, nickname: team2.nickname), wins: team2.wins, losses: team2.losses, percentage: winPercentage(wins: team2.wins, losses: team2.losses))
+        let team3data = TeamViewData(fullName: teamName(city: team3.city, nickname: team3.nickname), wins: team3.wins, losses: team3.losses, percentage: winPercentage(wins: team3.wins, losses: team3.losses))
         
         data.append(team1data)
         data.append(team2data)
@@ -51,5 +52,16 @@ class Presenter {
     
     func teamName(city: String, nickname: String) -> String {
         return "\(city) \(nickname)"
+    }
+    
+    func winPercentage(wins: Int, losses: Int) -> String {
+        let games = wins + losses
+        let percentage = Float(wins) / Float(games)
+        var formattedPercentage =  String(format: "%.3f", percentage)
+        
+        let index = formattedPercentage.index(formattedPercentage.startIndex, offsetBy: 1)
+        formattedPercentage = formattedPercentage.substring(from: index)
+        
+        return formattedPercentage
     }
 }
